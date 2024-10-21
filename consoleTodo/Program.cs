57290097450f3a74c6b1073840dcc339249f2ce9
chrisdovi <c.hristiandovidio@gmail.com>
2024-10-21 15:38:55 -0400

    Update Program.cs

--- consoleTodo/Program.cs
+++ consoleTodo/Program.cs
@@ -1,41 +1,32 @@
-﻿using Couchbase.Lite;
+using Couchbase.Lite;
+using Couchbase.Lite.Logging;
 using Couchbase.Lite.Query;
 using Couchbase.Lite.Sync;
 
-//setup the database
-var databaseConfig = new DatabaseConfiguration
-{
-    Directory = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)
-};
+Database.Log.Console.Domains = LogDomain.All; 
+Database.Log.Console.Level = LogLevel.Debug; 
 
 // Get the database (and create it if it doesn't exist)
-var database = new Database("mydb", databaseConfig);
+var database = new Database("mydb");
 
-// Create replicator to push and pull changes to and from the cloud
-// var targetEndpoint = new URLEndpoint(new Uri("wss://ujdiyqhabp-3f-lt.apps.cloud.couchbase.com:4984/location"));
-// var collection = database.CreateCollection("test", "inventory");
-
-// Create replicator to push and pull changes from docker
-var targetEndpoint = new URLEndpoint(new Uri("ws://localhost:4984/inventorydemo"));
+// Create replicator to push and pull changes to and from docker
+var targetEndpoint = new URLEndpoint(new Uri("ws://localhost:4984/audit"));
 var collection = database.CreateCollection("projects", "audit");
 
 var replConfig = new ReplicatorConfiguration(targetEndpoint);
 replConfig.AddCollection(collection);
 
-replConfig.Authenticator = new BasicAuthenticator("sync", "P@33w0rdS7nc");
+//authenticate with local user
+replConfig.Authenticator = new BasicAuthenticator("demo@example.com", "P@ssw0rd12");
+
 replConfig.Continuous = true;
 replConfig.ReplicatorType = ReplicatorType.PushAndPull;
 
-// Create replicator (make sure to add an instance or static variable
-// named _Replicator)
+// Create replicator (make sure to add an instance or static variable named _Replicator)
 var replicator = new Replicator(replConfig);
 replicator.AddChangeListener(OnReplicatorUpdate);
 
-try {
-    replicator.Start();
-} catch (Exception e) {
-    Console.WriteLine(e);
-}
+replicator.Start();
 
 // Create a query to fetch documents of type SDK
 // i.e. SELECT * FROM database
@@ -46,10 +37,15 @@ using var query = QueryBuilder.Select(SelectResult.All())
 var result = query.Execute();
 Console.WriteLine($"Number of rows :: {result.AllResults().Count}");
 
-Console.WriteLine("Press any key to stop...");
 Console.ReadKey();
 
 // Later, stop and dispose the replicator *before* closing/disposing the database
+
+while (true) 
+{
+    Console.WriteLine("Waiting");
+}
+
 void OnReplicatorUpdate(object sender, ReplicatorStatusChangedEventArgs e)
 {
     var status = e.Status;
